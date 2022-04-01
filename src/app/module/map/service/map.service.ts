8a57470cc27fef964d201e7e4666e8824b362def
Jérémy <jeremyperret43@gmail.com>
2022-04-01 16:56:11 +0200

    changes

--- src/app/module/map/service/map.service.ts
+++ src/app/module/map/service/map.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
 import { Location } from '../interface/map.interface';
 
 
